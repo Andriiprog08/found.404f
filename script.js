e21f10f8cf6f0278618fe081e692b5a264b024e3
Andriiprog08 <andriipalyvoda674@gmail.com>
2026-08-19 19:24:46 +0200

    feat: burger main links in header and media query for mobile responsiveness

---
+++ script.js
@@ -0,0 +1,21 @@
+const boxLinks= document.getElementById('box-links')
+const burgerBtn = document.getElementById('burgerBtn')
+const main = document.getElementById('main')
+
+burgerBtn.addEventListener('click', () => {
+  boxLinks.classList.toggle('active');
+  burgerBtn.classList.toggle('active');
+  main.classList.toggle('blurred');
+});
+
+document.addEventListener('click', (e) => {
+  const isMenuOpen = boxLinks.classList.contains('active');
+  const clickedInsideMenu = boxLinks.contains(e.target);
+  const clickedBurger = burgerBtn.contains(e.target);
+
+  if (isMenuOpen && !clickedInsideMenu && !clickedBurger) {
+    boxLinks.classList.remove('active');
+    burgerBtn.classList.remove('active');
+    main.classList.remove('blurred');
+  }
+});
