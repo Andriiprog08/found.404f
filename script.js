f38aeb6d6acd373f39395b973bbfd652c9bedb55
Andriiprog08 <andriipalyvoda674@gmail.com>
2026-08-19 20:00:50 +0200

    feat: add header in web whith fetch

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-const boxLinks= document.getElementById('box-links')
-const burgerBtn = document.getElementById('burgerBtn')
-const main = document.getElementById('main')
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
 
-burgerBtn.addEventListener('click', () => {
-  boxLinks.classList.toggle('active');
-  burgerBtn.classList.toggle('active');
-  main.classList.toggle('blurred');
-});
+        const boxLinks= document.getElementById('box-links')
+        const burgerBtn = document.getElementById('burgerBtn')
+        const main = document.getElementById('main')
+        
+        burgerBtn.addEventListener('click', () => {
+        boxLinks.classList.toggle('active');
+        burgerBtn.classList.toggle('active');
+        main.classList.toggle('blurred');
+        });
+        
+        document.addEventListener('click', (e) => {
+        const isMenuOpen = boxLinks.classList.contains('active');
+        const clickedInsideMenu = boxLinks.contains(e.target);
+        const clickedBurger = burgerBtn.contains(e.target);
+        
+        if (isMenuOpen && !clickedInsideMenu && !clickedBurger) {
+            boxLinks.classList.remove('active');
+            burgerBtn.classList.remove('active');
+            main.classList.remove('blurred');
+        }
+        });
+    });
 
-document.addEventListener('click', (e) => {
-  const isMenuOpen = boxLinks.classList.contains('active');
-  const clickedInsideMenu = boxLinks.contains(e.target);
-  const clickedBurger = burgerBtn.contains(e.target);
-
-  if (isMenuOpen && !clickedInsideMenu && !clickedBurger) {
-    boxLinks.classList.remove('active');
-    burgerBtn.classList.remove('active');
-    main.classList.remove('blurred');
-  }
-});
