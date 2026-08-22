a2dc8c6b5b6e17e654b17550c503d4ae543840a3
Andriiprog08 <andriipalyvoda674@gmail.com>
2026-08-22 21:30:02 +0200

    feat: add button burger for header.html and menu.html

--- script.js
+++ script.js
@@ -8,5 +8,16 @@ fetch('header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
+
+         const burgerBtn = document.getElementById('burgerBtn');
+        const currentPage = window.location.pathname.split('/').pop(); // например "menu.html" или "index.html"
+
+        if (currentPage === 'menu.html') {
+        burgerBtn.href = 'index.html';
+        burgerBtn.classList.add('active');
+        } else {
+        burgerBtn.href = 'menu.html';
+        burgerBtn.classList.remove('active');
+        }
     });
 
